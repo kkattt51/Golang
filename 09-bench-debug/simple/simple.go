636adbec43e730bf0907e7b45adf9bd4e1bb758f
kkattt51 <71519504+kkattt51@users.noreply.github.com>
2021-04-23 16:51:34 +0300

    Delete 09-bench-debug directory

--- 09-bench-debug/simple/simple.go
+++
@@ -1,9 +0,0 @@
-package simple
-
-import (
-	"math"
-)
-
-func trig(rad float64) float64 {
-	return math.Sin(math.Cos(rad))
-}
--- 09-bench-debug/simple/simple_test.go
+++
@@ -1,9 +0,0 @@
-package simple
-
-import "testing"
-
-func Benchmark_trig(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		trig(100)
-	}
-}
